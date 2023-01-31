c02eeeb2f2ec31689b631b8f487047ae33829d1f
Hollis Kwan <holliskwan.ios@gmail.com>
2023-01-30 21:34:10 -0800

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Hollis Kwan on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
